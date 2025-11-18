b3c2cb966ca81e5e1928fdd1b066ba2436f1fef9
Megan Mendoza <raw1218@gmail.com>
2025-11-18 17:53:59 -0500

    fixed deployment (pt2)

--- quantum-sim/vite.config.ts
+++ quantum-sim/vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 
 export default defineConfig({
-    base: '/QuantumCircuitSimulator/quantum-sim', // or '/quantum-sim/'
+    base: '/QuantumCircuitSimulator/', // or '/quantum-sim/'
     plugins: [react()],
 });
